7b89d87add4744c122e5a12a3e3a8e358fcda681
Gustavo Adolfo <gustavo.adolfo.as@gmail.com>
2022-01-29 20:34:33 -0300

    Ajustar o modelo para que os itens sejam associados a listas distintas

--- lists/models.py
+++ lists/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
+class List(models.Model):
+    name = models.CharField(max_length=100)
+    # items = models.ManyToManyField(Item)
+
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
